308a02bf74dce5cda93b98c1175c65b6d5eec641
Anh Tran <anhminhtran268@gmail.com>
2025-10-04 21:29:50 -0400

    changed properties to nullable

--- Models/EquipmentRequest.cs
+++ Models/EquipmentRequest.cs
@@ -9,24 +9,24 @@ namespace Assign1PROG30000.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Name is required.")]
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
         [Required(ErrorMessage = "Email is required.")]
         [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
-        public string Email { get; set; }
+        public string? Email { get; set; }
 
         [Required(ErrorMessage = "Phone number is required.")]
         [RegularExpression(@"^\d{3}-\d{3}-\d{4}$", ErrorMessage = "Phone number must be in the format xxx-xxx-xxxx.")]
-        public string Phone { get; set; }
+        public string? Phone { get; set; }
 
         [Required(ErrorMessage = "Role is required.")]
-        public string Role { get; set; }
+        public string? Role { get; set; }
 
         [Required(ErrorMessage = "Please select an equipment type.")]
-        public string EquipmentType { get; set; }
+        public string? EquipmentType { get; set; }
 
         [Required(ErrorMessage = "Request details are required.")]
-        public string RequestDetails { get; set; }
+        public string? RequestDetails { get; set; }
 
         [Range(1, int.MaxValue, ErrorMessage = "Duration must be greater than zero.")]
         public int Duration { get; set; }
